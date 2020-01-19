96d8fe1bb23758fc4e7be731e4e0164e7447d434
DBuckley0126 <DannyBoy.0126>
2020-01-19 23:19:39 +0000

    Add duplicate game protection

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -8,20 +8,22 @@ class GameChannel < ApplicationCable::Channel
         ActionCable.server.broadcast(
           "game_channel_#{game.join_code}",
           action: "game_join_success",
-          type: "alert",
+          type: "subscribed",
           body: {message: "mobile controller successfully joined game #{game.join_code} and subscribed"}
         )
       else
         reject
       end
     else
+      reject if Game.find_by(join_code: params[:join_code])
+      
       game = Game.new(join_code: params[:join_code])
       game.save
       stream_from "game_channel_#{game.join_code}"
       ActionCable.server.broadcast(
         "game_channel_#{game.join_code}",
         action: "game_create_success",
-        type: "alert",
+        type: "subscribed",
         body: {message: "successfully created game #{game.join_code} and subscribed"}
       )
     end
@@ -36,7 +38,7 @@ class GameChannel < ApplicationCable::Channel
       ActionCable.server.broadcast(
          "game_channel_#{params[:join_code]}",
          action: "data_relay",
-         type: "broadcast",
+         type: "data_relay",
          body: data["body"]
        )
   end
